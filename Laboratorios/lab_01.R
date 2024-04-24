139a170325b27466d77d7281a9b78394adb68e49
Marco <marcgonz@yahoo.com>
2024-04-24 10:45:33 -0600

    Crear carpeta y lab_01

---
+++ Laboratorios/lab_01.R
@@ -0,0 +1,19 @@
+# Marco Aurelio González Tagle
+# Matrícula
+# 24/04/2024
+
+# Laboratorio 1
+
+# gastos totales
+
+300 + 240 + 1527 + 1500 + 1833
+
+# Crear objetos con valor
+
+celular <- 300
+
+# Operaciones con objetos
+
+celular + celular # suma
+celular * 3 # multiplicación
+celular / 2 # división
